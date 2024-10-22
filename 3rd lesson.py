4bdb3e5d158a807bb233d9302789647b05e72377
sugd05 <abdulkayumikhtiyorov@gmail.com>
2024-10-22 19:46:35 +0500

    Переменные

---
+++ 3rd lesson.py
@@ -0,0 +1,5 @@
+homework_count = 12
+past_hours = 1.5
+course_name = 'Python'
+time_for_an_exercise = past_hours / homework_count
+print('Курс:',course_name,", всего задач:",homework_count,", затрачено часов:",past_hours,", среднее время выполнения", time_for_an_exercise, "часа.")
