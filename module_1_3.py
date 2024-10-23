1d9a02d21b917ea7a16493edbadfca5d4e173b70
sugd05 <abdulkayumikhtiyorov@gmail.com>
2024-10-23 17:33:17 +0500

    Update module_1_3.py
    Changed. Thx for comment:)

--- module_1_3.py
+++ module_1_3.py
@@ -2,7 +2,7 @@ name = 'Abdul'
 print(name)
 age = 17
 print(age)
-new_age = int('19')
-print(age + new_age)
-Is_Student = (7>=5 and 0.5>-4)
+age = age + 1
+print(age)
+Is_Student = (True)
 print(Is_Student)
