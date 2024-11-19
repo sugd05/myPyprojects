b98c66cb9477131612d3c43fded878a2f98880da
sugd05 <abdulkayumikhtiyorov@gmail.com>
2024-11-19 20:34:02 +0500

    Add files via upload

---
+++ mdule_2_4.py
@@ -0,0 +1,18 @@
+primes = []
+not_primes = []
+numbers = [1, 2, 3, 4, 5, 6, 7,
+  8, 9, 10, 11, 12, 13, 14, 15]
+
+for num in numbers:
+    is_prime = True
+    for i in range(2, int(num ** 0.5) + 1):
+        if num % i == 0:
+            is_prime = False
+            break
+    if is_prime:
+        primes.append(num)
+    else:
+        not_primes.append(num)
+
+print(primes)
+print(not_primes)
